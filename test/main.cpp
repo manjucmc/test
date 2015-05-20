c1187f3113bc5a3d1317cced76d0e97e83a7662b
manjucmc <manju_cmc@yahoo.com>
2015-05-19 22:15:15 -0700

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Manju Balasubramaniam on 5/19/15.
+//  Copyright (c) 2015 Manju Balasubramaniam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
